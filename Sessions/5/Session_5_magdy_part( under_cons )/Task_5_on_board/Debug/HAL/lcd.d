19699729e7926adc1feff0dc8c3c34cda26149aa
Mohamed Magdy <62652769+mohamedmagdy484@users.noreply.github.com>
2026-02-06 15:44:40 +0200

    Add files via upload

---
+++ Sessions/5/Session_5_magdy_part( under_cons )/Task_5_on_board/Debug/HAL/lcd.d
@@ -0,0 +1,13 @@
+HAL/lcd.o HAL/lcd.o: ../HAL/lcd.c ../HAL/../COMMON/common_macros.h \
+  ../HAL/lcd.h ../HAL/../COMMON/std_types.h ../HAL/../MCAL/gpio.h \
+  ../HAL/../MCAL/../common/std_types.h
+
+../HAL/../COMMON/common_macros.h:
+
+../HAL/lcd.h:
+
+../HAL/../COMMON/std_types.h:
+
+../HAL/../MCAL/gpio.h:
+
+../HAL/../MCAL/../common/std_types.h:
