fa6fcdc523960f48b6dbb258958b52b7d3f98b3d
FatmaFoley <foleyfatma120@gmail.com>
2026-07-16 00:09:47 +0300

    Refactor I2C and LCD function names in main.c
    Updated I2C and LCD function calls for improved clarity.

--- Sessions/10/Lab/Code/main.c
+++ Sessions/10/Lab/Code/main.c
@@ -3,35 +3,45 @@
  *
  * Created: 2/9/2026 7:34:37 PM
  * Author : fatma foley
- */ 
- 
+ */
+
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
-#include "i2c.h"
-#include "LCD_I2C.h"
-
+#include "twi.h"
+#include "lcd.h"
 
 int main(void)
 {
+    // Initialize the I2C peripheral
     i2c_init();
-	i2c_start();
-	i2c_write(0x70);
-	lcd_init();
-	
-    while (1) 
+
+    // Generate I2C start condition
+    i2c_start();
+
+    // Send the PCF8574A slave address
+    i2c_sendByte(0x70);
+
+    // Initialize the LCD
+    lcd_init();
+
+    while (1)
     {
-		lcd_cmd(0x80);
-		lcd_msg("IEEE LastSession");
-		_delay_ms(1000);
-		lcd_cmd(0xC0);
-		_delay_ms(1000);
-		lcd_msg("We are Using ");
-		lcd_msg("I2");
-		lcd_msg("C");
-		_delay_ms(1000);
-		lcd_cmd(0x01);
-		_delay_ms(1000);
+        // Move cursor to the first row
+        lcd_cmd(0x80);
+        lcd_display_string("IEEE LastSession");
+        _delay_ms(1000);
+
+        // Move cursor to the second row
+        lcd_cmd(0xC0);
+        _delay_ms(1000);
+
+        // Display text on the second row
+        lcd_display_string("We are Using I2C");
+        _delay_ms(1000);
+
+        // Clear the LCD
+        lcd_cmd(0x01);
+        _delay_ms(1000);
     }
 }
-
